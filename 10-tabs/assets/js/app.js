f7332481d578526fe96c16374dab737a3b39adf3
Sabuhi0 <sabuhiq0@gmail.com>
2022-02-05 23:23:06 +0400

    Add tabs project

--- 10-tabs/assets/js/app.js
+++ 10-tabs/assets/js/app.js
@@ -1,1 +1,18 @@
-console.log("Hi tabs project")
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function(e) {
+    const id = e.target.dataset.id;
+    if (id) {
+        btns.forEach(function(btn) {
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        articles.forEach(function(article) {
+            article.classList.remove("active");
+        });
+        const elemet = document.getElementById(id);
+        elemet.classList.add("active")
+    };
+});
