74d50f5838ce4f9e44819715f705e6690c0d1f3b
Sabuhi0 <sabuhiq0@gmail.com>
2022-02-08 22:12:52 +0400

    Enter the value of the console entry when you press the button

--- 13-grocery-bud/assets/js/app.js
+++ 13-grocery-bud/assets/js/app.js
@@ -5,3 +5,16 @@ const submitBtn = document.querySelector(".submit-btn");
 const container = document.querySelector(".grocery-container");
 const list = document.querySelector(".grocery-list");
 const clearBtn = document.querySelector(".clear-btb");
+
+// Edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// Submit Form
+form.addEventListener("submit", addItem);
+
+function addItem(e) {
+    e.preventDefault();
+    console.log(grocery.value)
+}
